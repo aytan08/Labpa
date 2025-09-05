85828989eaa001fae33b89b34280b5828d5a8818
Aytan Z. Dadashova <aytanzd@code.local>
2025-09-05 15:41:55 +0400

    Labpa201

---
+++ Pa201lab/Pa201lab/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*string name = "Ayten";
+Console.WriteLine("Salam " + name);*/
+using System.ComponentModel.Design;
+
+/* int num = 9;
+if (num > 0)
+{
+   Console.WriteLine("Positive");
+}
+else if (num == 0) {
+   Console.WriteLine("Zero");
+}
+else
+{ 
+   Console.WriteLine("Negative");
+} */
+
+int age = 43;
+if (age<18&&age>0)
+{
+    Console.WriteLine("Usaq");
+}
+else if (age<65&&age>18)
+{
+    Console.WriteLine("Yetkin");
+}
+else if (age>65)
+        {
+    Console.WriteLine("Yasli");
+}
+      
